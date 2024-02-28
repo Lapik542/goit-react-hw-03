27e6385de0ca6a6d7009de70c56774a65f64b941
Гавриш Олександр <132341505+Lapik542@users.noreply.github.com>
2024-02-28 14:39:09 +0200

    main

---
+++ src/TextInput.jsx
@@ -0,0 +1,10 @@
+export default function TextInput({value, onChange}) {
+   return (
+      <>
+         <input
+            type="text"
+            value={value}
+            onChange={(e) => onChange(e.target.value)} />
+      </>
+   )
+};
